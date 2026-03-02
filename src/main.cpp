169f6f799a764cad5aa191b50c93d06f7bf74e8c
milou! <m.dezwaan@hotmail.nl>
2026-03-02 09:53:05 +0100

    gitignore added

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-//==============================================================
-// Filename    : 
-// Authors     : 
-// Group       :
-// License     :  N.A. or opensource license like LGPL
-// Description : 
-//==============================================================
+//=====================...........................=================================
+// Authors : Milou de Zwaan (2629070), Shatrunjay Palkar (3669734)
+// Group : 14
+// License : LGPL open source license
+//
+// Brief : 
+//=====================...........................=================================
+
+#include "List.h"
 
 int main() {    
     return 0;
+    List<char> charList; // create List of char
 }
