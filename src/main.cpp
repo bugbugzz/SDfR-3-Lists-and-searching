c6272f2affd3a15f7769f56ae238c5af6a7211d4
shatrunjayp2000-dotcom <shatrunjayp2000@gmail.com>
2026-03-04 16:05:39 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,46 @@
 #include <string>
 #include "List.h"
 
-template<typename NODETYPE>
+//Function to fill list with string
+void filllist(std::string s, List<char>& l){
 
-void filllist(std::string s, List<NODETYPE>& l){
-    for(NODETYPE i: s)
+    //Iterate over each element of the string
+    for(char i: s)
     {
+        //Insert values at the back of list
         l.insertAtBack(i);
     }
 
 }
 
-
 int main() {
     
-    List<char> list_1;
-    List<char> list_2;
+    List<char> list_1; //Creating empty list of type char
+    List<char> list_2; //Creating empty list of type char
 
+    //Fill lists 1 and 2 with strings
     filllist("singlylinkedlist", list_1);
     filllist("abcdefg", list_2);
 
+    //Display list_1 and list_2
     list_1.print();
     list_2.print();
 
-    List<char> list_3;
-    List<char> list_4;
+    List<char> list_3; //Creating empty list of type char
+    List<char> list_4; //Creating empty list of type char
 
+    //Fill lists 3 and 4 with strings
     filllist("hijklmnop", list_3);
     filllist("qrstuvw", list_4);
 
+    //Concatenate list_3 to list_2 and empty list_3
     list_2.concatenate(list_3);
+    //Concatenate list_4 to list_2 and empty list_4
     list_2.concatenate(list_4);
 
+    //Display list_2
     list_2.print();
-    list_3.print();
-    list_4.print();
-
-
+    
     return 0;
 }
+
