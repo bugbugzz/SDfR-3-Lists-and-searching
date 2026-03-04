ac6fb3275f60b3d879a63c68238b33e54d017444
shatrunjayp2000-dotcom <shatrunjayp2000@gmail.com>
2026-03-04 12:02:51 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,12 @@
 #include <string>
 #include "List.h"
 
-void filllist(std::string s, List<char> *l){
-    for(char i: s)
+template<typename NODETYPE>
+
+void filllist(std::string s, List<NODETYPE>& l){
+    for(NODETYPE i: s)
     {
-        l->insertAtBack(i);
+        l.insertAtBack(i);
     }
 
 }
@@ -24,12 +26,25 @@ int main() {
     List<char> list_1;
     List<char> list_2;
 
-    filllist("singlylinkedlist", &list_1);
-    filllist("abcdefg", &list_2);
+    filllist("singlylinkedlist", list_1);
+    filllist("abcdefg", list_2);
 
     list_1.print();
     list_2.print();
 
+    List<char> list_3;
+    List<char> list_4;
+
+    filllist("hijklmnop", list_3);
+    filllist("qrstuvw", list_4);
+
+    list_2.concatenate(list_3);
+    list_2.concatenate(list_4);
+
+    list_2.print();
+    list_3.print();
+    list_4.print();
+
+
     return 0;
 }
-
